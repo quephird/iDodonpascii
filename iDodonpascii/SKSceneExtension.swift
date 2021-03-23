008be10ee9671eae91a73572b80d02e7aaed5532
quephird <danielle_kefford@hotmail.com>
2021-03-22 17:24:58 -0700

    Changed scaling strategy for both start and level background images.

--- iDodonpascii/SKSceneExtension.swift
+++ iDodonpascii/SKSceneExtension.swift
@@ -6,12 +6,8 @@
 import SpriteKit
 
 extension SKScene {
-//    func computeProperScale(scene: SKScene, backgroundImage: SKSpriteNode) -> CGFloat {
-//        return max(scene.size.width/backgroundImage.size.width,
-//                   scene.size.height/backgroundImage.size.height)
-//    }
     func computeProperScale(backgroundImage: SKSpriteNode) -> CGFloat {
-        return max(self.size.width/backgroundImage.size.width,
+        return min(self.size.width/backgroundImage.size.width,
                    self.size.height/backgroundImage.size.height)
     }
 }
